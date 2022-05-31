11071d8fa42a92dd995019c203b5f332536577ce
ULTRONVIKASH <vickyanga@gmail.com>
2022-05-31 17:23:36 +0530

    conflict created

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,4 @@
-const router= require('express').Router();
-const User = require('../model/User')
+
 const newLocal = '/register'
 router.post(newLocal,async (req,res)=>{
     const user = new User({
